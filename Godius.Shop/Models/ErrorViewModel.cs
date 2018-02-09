97d907484f1765292935e26492f46bb7b1797a0c
Reasty Kim <reastykim@orthosensor.com>
2018-02-09 14:36:04 +0900

    Add project files.

---
+++ Godius.Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Godius.Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
