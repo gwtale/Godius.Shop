13a640fd87391a72b4e92709ef53ace4d0beb1e9
Reasty Kim <reastykim@orthosensor.com>
2018-02-13 11:10:01 +0900

    Updated GoodsManagement views

--- Godius.Shop/Controllers/GoodsManagementController.cs
+++ Godius.Shop/Controllers/GoodsManagementController.cs
@@ -80,7 +80,7 @@ namespace Godius.Shop.Controllers
         }
 
 		// GET: GoodsManagement/EditCreateCategory/5
-		public async Task<IActionResult> EditCreateCategory(Guid? id)
+		public async Task<IActionResult> EditCategory(Guid? id)
         {
             if (id == null)
             {
@@ -100,7 +100,7 @@ namespace Godius.Shop.Controllers
 		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditCreateCategory(Guid id, [Bind("Id,SerialCode,Name")] GoodsCategory goodsCategory)
+        public async Task<IActionResult> EditCategory(Guid id, [Bind("Id,SerialCode,Name")] GoodsCategory goodsCategory)
         {
             if (id != goodsCategory.Id)
             {
@@ -131,7 +131,7 @@ namespace Godius.Shop.Controllers
         }
 
 		// GET: GoodsManagement/DeleteCreateCategory/5
-		public async Task<IActionResult> DeleteCreateCategory(Guid? id)
+		public async Task<IActionResult> DeleteCategory(Guid? id)
         {
             if (id == null)
             {
@@ -149,9 +149,9 @@ namespace Godius.Shop.Controllers
         }
 
 		// POST: GoodsManagement/DeleteCreateCategory/5
-		[HttpPost, ActionName("Delete")]
+		[HttpPost, ActionName("DeleteCategory")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        public async Task<IActionResult> DeleteCategoryConfirmed(Guid id)
         {
             var goodsCategory = await _context.GoodsCategories.SingleOrDefaultAsync(m => m.Id == id);
             _context.GoodsCategories.Remove(goodsCategory);
@@ -168,7 +168,7 @@ namespace Godius.Shop.Controllers
 
 		#region Goods
 
-		// GET: Goods/CreateGoods
+		// GET: GoodsManagement/CreateGoods
 		public async Task<IActionResult> CreateGoods()
 		{
 			ViewBag.Categories = await _context.GoodsCategories
@@ -182,7 +182,7 @@ namespace Godius.Shop.Controllers
 			return View();
 		}
 
-		// POST: Goods/CreateGoods
+		// POST: GoodsManagement/CreateGoods
 		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
 		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
@@ -224,6 +224,141 @@ namespace Godius.Shop.Controllers
 				return RedirectToAction(nameof(Index));
 			}
 			return View(model);
+		}
+
+		// GET: GoodsManagement/DetailsGoods/5
+		public async Task<IActionResult> DetailsGoods(Guid? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var goods = await _context.Goods
+				.SingleOrDefaultAsync(m => m.Id == id);
+			if (goods == null)
+			{
+				return NotFound();
+			}
+
+			return View(goods);
+		}
+
+		// GET: GoodsManagement/EditGoods/5
+		public async Task<IActionResult> EditGoods(Guid? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var goods = await _context.Goods.SingleOrDefaultAsync(m => m.Id == id);
+			if (goods == null)
+			{
+				return NotFound();
+			}
+			return View(new EditGoodsViewModel { Id = goods.Id, SerialCode = goods.SerialCode, Name = goods.Name, Price = goods.Price, ImagePath = goods.Image });
+		}
+
+		// POST: GoodsManagement/EditGoods/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> EditGoods(Guid id, [FromForm] EditGoodsViewModel model)
+		{
+			if (id != model.Id)
+			{
+				return NotFound();
+			}
+
+			var goods = await _context.Goods.SingleOrDefaultAsync(m => m.Id == id);
+			if (goods == null)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					if (model.Image?.Length > 0)
+					{
+						var dirPath = Path.Combine(_hostingEnvironment.WebRootPath, _appOptions.GoodsImagePath);
+						var dirInfo = new DirectoryInfo(dirPath);
+						if (dirInfo.Exists != true)
+						{
+							dirInfo.Create();
+						}
+
+						var fileName = Path.GetFileName(model.Image.FileName);
+						var fileFullPath = Path.Combine(dirInfo.FullName, fileName);
+						using (var stream = new FileStream(fileFullPath, FileMode.Create))
+						{
+							await model.Image.CopyToAsync(stream);
+						}
+
+						// Update goods image
+						goods.Image = "/" + Path.Combine(_appOptions.GoodsImagePath, fileName);
+						model.ImagePath = goods.Image;
+					}
+
+					// Update goods info
+					goods.SerialCode = model.SerialCode;
+					goods.Name = model.Name;
+					goods.Price = model.Price;
+
+					_context.Update(goods);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!GoodsExists(model.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(model);
+		}
+
+		// GET: GoodsManagement/DeleteGoods/5
+		public async Task<IActionResult> DeleteGoods(Guid? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var goods = await _context.Goods
+				.SingleOrDefaultAsync(m => m.Id == id);
+			if (goods == null)
+			{
+				return NotFound();
+			}
+
+			return View(goods);
+		}
+
+		// POST: GoodsManagement/DeleteGoods/5
+		[HttpPost, ActionName("DeleteGoods")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteGoodsConfirmed(Guid id)
+		{
+			var goods = await _context.Goods.SingleOrDefaultAsync(m => m.Id == id);
+			_context.Goods.Remove(goods);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool GoodsExists(Guid id)
+		{
+			return _context.Goods.Any(e => e.Id == id);
 		}
 
 		#endregion
