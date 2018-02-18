141b3f394dae3afeb8c1e704b4c0418a5fbce5c6
Reasty Kim <reastykim@orthosensor.com>
2018-02-18 16:40:22 +0900

    Updated the Login, Register viewmodel Display

--- Godius.Shop/Models/AccountViewModels/LoginViewModel.cs
+++ Godius.Shop/Models/AccountViewModels/LoginViewModel.cs
@@ -8,15 +8,17 @@ namespace Godius.Shop.Models.AccountViewModels
 {
     public class LoginViewModel
     {
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+		[Required(ErrorMessage = "이메일 아이디를 입력하세요.")]
+		[EmailAddress]
+		[Display(Name = "이메일 아이디")]
+		public string Email { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+		[Required(ErrorMessage = "비밀번호를 입력하세요.")]
+		[DataType(DataType.Password)]
+		[Display(Name = "비밀번호")]
+		public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "아이디 저장")]
         public bool RememberMe { get; set; }
     }
 }
--- Godius.Shop/Models/AccountViewModels/RegisterViewModel.cs
+++ Godius.Shop/Models/AccountViewModels/RegisterViewModel.cs
@@ -8,20 +8,20 @@ namespace Godius.Shop.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
+        [Required(ErrorMessage = "이메일 주소를 입력하세요.")]
+        [EmailAddress(ErrorMessage = "올바른 이메일 주소 형식이 아닙니다.")]
+        [Display(Name = "이메일")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
+        [Required(ErrorMessage = "비밀번호를 입력하세요.")]
+        [StringLength(100, ErrorMessage = "{0}는 {2}자 이상 {1} 미만으로 설정 가능합니다.", MinimumLength = 4)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "비밀번호")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "비밀번호 확인")]
+        [Compare("Password", ErrorMessage = "비밀번호와 일치하지 않습니다.")]
         public string ConfirmPassword { get; set; }
     }
 }
